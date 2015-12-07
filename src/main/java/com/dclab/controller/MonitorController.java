f52d2d6df29476fcb3f3a79001a68c69f2ded191
ernestyj <ernestyj@outlook.com>
2015-12-07 19:53:22 +0800

    切换使用jdk8

--- src/main/java/com/dclab/controller/MonitorController.java
+++ src/main/java/com/dclab/controller/MonitorController.java
@@ -7,13 +7,11 @@ import com.dclab.service.MonitorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 /**
  * Created by Kaiyang Lv on 12/4/15.
@@ -29,9 +27,9 @@ public class MonitorController {
         return new ResponseResult<MonitorStatus>(monitorService.getMonitorInfo(body.getCommId()));
     }
 
-//    @RequestMapping(value = "/mobile/user/monitor", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+//    @RequestMapping(value = "/mobile/user/monitor", method = RequestMethod.POST)
 //    @ResponseBody
-//    public Collection<MonitorStatus> getMonitorInfo(int communityId) {
+//    public List<MonitorStatus> getMonitorInfo(int communityId) {
 //        return monitorService.getMonitorInfo(communityId);
 //    }
 }
